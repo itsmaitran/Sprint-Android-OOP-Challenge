d264747d145bf5c35ec3bd8471327129222afd10
itsmaisn <itsmaisn@gmail.com>
2019-09-13 13:44:29 -0700

    Made some changes to the text that is displayed.

--- app/src/main/java/com/lambdaschool/sprintandroidoopchallenge/model/Structures.kt
+++ app/src/main/java/com/lambdaschool/sprintandroidoopchallenge/model/Structures.kt
@@ -14,7 +14,7 @@ class Structures (
 
 ) : AgeOfEmpires(name, expansion, isFavorite) {
 
-    override fun info(): String = "Expansion: $expansion\nBuild Time: $buildTime\nHit Points: $hitPoints\nLine of Sight: $lineOfSight\nArmor: $armor"
+    override fun info(): String = "Structure: $name\nExpansion: $expansion\nBuild Time: $buildTime\nHit Points: $hitPoints\nLine of Sight: $lineOfSight\nArmor: $armor"
 
     override fun toString(): String {
         return "$name has a build time of $buildTime, hit points of $hitPoints, a line of sight of $lineOfSight, and an armor rating of $armor."
